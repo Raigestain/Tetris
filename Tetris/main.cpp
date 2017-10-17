b58529785d4c9b6991e07ddcdc8c38dda8267d92
Raigestain <Raigestain@users.noreply.github.com>
2017-10-17 17:13:48 -0500

    Update main.cpp

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -16,19 +16,22 @@ int main(int argc, char* argv[])
 	Uint32 start_time = 0;
 
 	Uint32 counter = 0;
+	Uint32 deltaTime = 0;
 
 	while (!window.isClosed())
 	{
 		start_time = SDL_GetTicks();
-
-		window.PollEvents();
-		window.Render();
 
 		if (FPS > (SDL_GetTicks() - start_time))
 		{
 			SDL_Delay(FPS);
 		}
 
+		deltaTime = (SDL_GetTicks() - start_time);
+
+		window.PollEvents();
+		window.Update(deltaTime);
+		window.Render();
 	}
 
 	return 0;
