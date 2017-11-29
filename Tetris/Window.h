46c97b372488d31ab5290fc9e50df4900d19ba12
Raigestain <Raigestain@users.noreply.github.com>
2017-11-29 17:06:17 -0600

    Updated the Window.h file to match with the cpp.
    I forgot to commit the .h file in a previous commit.

--- Tetris/Window.h
+++ Tetris/Window.h
@@ -6,14 +6,18 @@
 #include <iostream>
 #include "Cleanup.h"
 
-const int SCREEN_WIDTH     = 1024; //!< Width of the window screen
-const int SCREEN_HEIGHT    = 920;  //!< Height of the window screen.
-const Uint32 GROUND_WIDTH  = 10;   //!< Width of the play ground.
-const Uint32 GROUND_HEIGHT = 20;   //!< Height of the play ground.
-const float TIME_SPAN      = 750;  //!< Number of ms for ground update.
-const short SHAPE_SIZE     = 25;   //!< Size of the array containing the piece.
-const short SHAPE_STATES   = 4;    //!< Number of the shapes' states.
-const short SHAPE_NUM      = 7;    //!< Number of different pieces.
+const int SCREEN_WIDTH = 1024;   //!< Width of the window screen
+const int SCREEN_HEIGHT = 920;   //!< Height of the window screen.
+const Uint32 GROUND_WIDTH = 10;  //!< Width of the play ground.
+const Uint32 GROUND_HEIGHT = 20; //!< Height of the play ground.
+const float TIME_SPAN = 750;     //!< Number of ms for ground update.
+const short SHAPE_OFFSET = 5;    //!< The row offset for 
+const short SHAPE_SIZE = 25;     //!< Size of the array containing the piece.
+const short SHAPE_STATES = 4;    //!< Number of the shapes' states.
+const short SHAPE_NUM = 7;       //!< Number of different pieces.
+const short START_X = 3;         //!< Starting x position of the piece.
+const short START_Y = 1;         //!< Starting y position of the piece.
+
 
 using std::cout;
 using std::cerr;
@@ -34,44 +38,48 @@ struct GROUND
 class Window
 {
 public:
-	Window(const string& title, unsigned short width, unsigned short height);
-	~Window();
+  Window(const string& title, unsigned short width, unsigned short height);
+  ~Window();
 
-  
-	inline bool isClosed() const { return m_isClosed; }
 
-	void PollEvents();
-	void Update(float deltaTime);
-	void Render();
+  inline bool isClosed() const { return m_isClosed; }
+
+  void PollEvents();
+  void Update(float deltaTime);
+  void Render();
 
 private:
   //Window functions.
-	bool Init();
-	void Destroy();
-	void ProcessKeys(const SDL_KeyboardEvent &kbEvent);
+  bool Init();
+  void Destroy();
+  void ProcessKeys(const SDL_KeyboardEvent &kbEvent);
 
   //Game functions.
   void GameInit();
-	void UpdateGround(float deltaTime);
-	void RenderGround();
-	void SpawnPiece();
-	void LockPiece();
+  void UpdateGround(float deltaTime);
+  void RenderGround();
+  void SpawnPiece();
+  void MoveDown();
+  void LockPiece();
 
 private:
   //Window variables.
-	string m_title;
-	unsigned short m_height;
-	unsigned short m_width;
-	SDL_Window* m_pWindow;
-	SDL_Renderer* m_pRenderer;
-	SDL_Texture* m_pTexture;
+  string m_title;
+  unsigned short m_height;
+  unsigned short m_width;
+  SDL_Window* m_pWindow;
+  SDL_Renderer* m_pRenderer;
+  SDL_Texture* m_pTexture;
   bool m_isClosed;
 
   //Game variables.
   float m_timer;
-	short m_actualState;
-	char m_activePiece[SHAPE_STATES][SHAPE_SIZE];
+  short m_actualState;
+  char m_activePiece[SHAPE_STATES][SHAPE_SIZE];
   bool m_locked;
+  short m_piecePosX;
+  short m_piecePosY;
+
 };
 
 #endif
