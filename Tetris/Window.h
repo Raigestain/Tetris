3b669d954365fa487fd863ee45b130551d9b202f
Raigestain <Raigestain@users.noreply.github.com>
2017-11-30 17:32:05 -0600

    Added new members for the class
    Added the Background texture and a new flag to let the new spawned piece stay a little while on the top.
    Modified m_locked to be a char to have more than 2 states.

--- Tetris/Window.h
+++ Tetris/Window.h
@@ -10,13 +10,13 @@ const int SCREEN_WIDTH = 1024;   //!< Width of the window screen
 const int SCREEN_HEIGHT = 920;   //!< Height of the window screen.
 const Uint32 GROUND_WIDTH = 10;  //!< Width of the play ground.
 const Uint32 GROUND_HEIGHT = 20; //!< Height of the play ground.
-const float TIME_SPAN = 750;     //!< Number of ms for ground update.
+const float TIME_SPAN = 600;     //!< Number of ms for ground update.
 const short SHAPE_OFFSET = 5;    //!< The row offset for 
 const short SHAPE_SIZE = 25;     //!< Size of the array containing the piece.
 const short SHAPE_STATES = 4;    //!< Number of the shapes' states.
 const short SHAPE_NUM = 7;       //!< Number of different pieces.
 const short START_X = 3;         //!< Starting x position of the piece.
-const short START_Y = 1;         //!< Starting y position of the piece.
+const short START_Y = 0;         //!< Starting y position of the piece.
 
 
 using std::cout;
@@ -60,6 +60,7 @@ private:
   void RenderGround();
   void SpawnPiece();
   void MoveDown();
+  void RollBack(short rowsCount);
   void LockPiece();
 
 private:
@@ -70,15 +71,17 @@ private:
   SDL_Window* m_pWindow;
   SDL_Renderer* m_pRenderer;
   SDL_Texture* m_pTexture;
+  SDL_Texture* m_pBckTexture;
   bool m_isClosed;
 
   //Game variables.
   float m_timer;
   short m_actualState;
-  char m_activePiece[SHAPE_STATES][SHAPE_SIZE];
-  bool m_locked;
   short m_piecePosX;
   short m_piecePosY;
+  unsigned char m_activePiece[SHAPE_STATES][SHAPE_SIZE];
+  unsigned char m_locked;
+  bool m_newPiece; //Use this to let the piece be in the top part of the ground.
 
 };
 
